6839aa5e9c7df0b2bd2946116960c18b6d0d577a
kimjw <kimjw@192.168.42.249>
2022-02-24 20:46:30 +0900

    api 문서 업데이트

--- src/main/java/com/jwkim/idus/RootController.java
+++ src/main/java/com/jwkim/idus/RootController.java
@@ -19,6 +19,8 @@ import com.jwkim.idus.dto.User;
 import com.jwkim.idus.mapper.readApi.ReadApiMapper;
 import com.jwkim.idus.service.CommonService;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
@@ -33,7 +35,9 @@ public class RootController {
 
 	static int PAGE_SIZE = 10;
 	
-	
+	@ApiOperation(
+			value="swagger-ui 페이지",
+			notes="swagger-ui 페이지로 리다이렉션 합니다.")
 	@GetMapping("/")
 	public ModelAndView baseDir() {		
 		
@@ -41,7 +45,11 @@ public class RootController {
     }
 		
 	
-	@ApiOperation(value="회원 가입")
+	@ApiOperation(
+			value="회원 가입",
+			notes=
+			"- 경로 변수로 주어진 사용자를 추가(회원 가입)합니다.\r\n" +
+			"- 사용자 정보는 request body에 json 형태로 담아야 합니다.")
 	@PostMapping("/user/{id}")
 	public boolean insertUser(HttpServletRequest request, @PathVariable String id, @RequestBody User user) {
 		
@@ -50,34 +58,72 @@ public class RootController {
 		return true;
     }
 	
-	@ApiOperation(value="단일 회원 상세 정보 조회")
+	@ApiOperation(
+			value="단일 회원 상세 정보 조회",
+			notes="경로 변수로 주어진 사용자 정보를 조회합니다.")
 	@GetMapping("/user/{id}")
 	public User findUser(@PathVariable String id) {
 		return mapper.findUser(id);
 	}
 	
-	@ApiOperation(value="단일 회원의 주문 목록 조회")
-	@GetMapping("/user/{id}/order")
+	@ApiOperation(
+			value="단일 회원의 주문 목록 조회",
+			notes="경로 변수로 주어진 사용자의 모든 주문 정보를 조회합니다.")
 	public List<Order> findUserOrderList(@PathVariable String id) {
 		return mapper.findUserOrderList(id);
 	}
 		
-	@ApiOperation(value="여러 회원 목록 조회", notes=
-			"\t- 페이지네이션으로 10건 단위로 조회합니다.\r\n" + 
-			"\t- 이름, 이메일을 이용하여 검색이 가능합니다.\r\n" + 
-			"\t- 각 회원의 마지막 주문 정보를 포함합니다.")
+	@ApiOperation(
+			value="여러 회원 목록 조회",
+			notes=
+			"\t- 주어진 페이지에 해당하는 데이터를 10건 단위로 조회합니다.\r\n" + 
+			"\t- 이름, 이메일을 이용하여 like 검색이 가능합니다.\r\n" + 
+			"\t- 각 회원의 마지막 주문 번호를 포함합니다.\r\n" +
+			"\tex. http://localhost:8080/user?page=1&user_name=테스트&email_addr=idus.com"
+			)
+	@ApiImplicitParams(
+			{
+				@ApiImplicitParam(
+				        name = "page",
+				        value = "페이지",
+				        required = false,
+				        dataType = "int",
+				        paramType = "query",
+				        defaultValue = "None"),
+				@ApiImplicitParam(
+				        name = "user_name",
+				        value = "사용자명",
+				        required = false,
+				        dataType = "string",
+				        paramType = "query",
+				        defaultValue = "None"),
+				@ApiImplicitParam(
+				        name = "email_addr",
+				        value = "이메일",
+				        required = false,
+				        dataType = "string",
+				        paramType = "query",
+				        defaultValue = "None"),
+			}
+			)
+	
 	@GetMapping("/user")
 	public List<User> findUserList(@RequestParam Map<String, Object> params) {
 		
 		// 페이징 파라메터 설정
-		Object page  = params.get("page");
-		if(page !=null && Integer.valueOf(String.valueOf(page)) >= 1) {
-			params.put("start", (Integer.valueOf(String.valueOf(page))-1) * 10);
-		} else {
+		try {
+			int page = Integer.valueOf((String)params.get("page"));
+			if(page >= 1) {
+				params.put("start", (page-1) * PAGE_SIZE);
+			} else {
+				params.put("start", 0);
+			};
+		} catch (Exception e){
 			params.put("start", 0);
-		};
-		params.put("page_size", PAGE_SIZE);
+		}
 		
+		// 페이지 크기 설정
+		params.put("page_size", PAGE_SIZE);		
 		
 		return mapper.findUserList(params);
 	}
--- src/main/java/com/jwkim/idus/framework/swaggerui/SwaggerConfig.java
+++ src/main/java/com/jwkim/idus/framework/swaggerui/SwaggerConfig.java
@@ -1,9 +1,13 @@
 package com.jwkim.idus.framework.swaggerui;
 
+import java.util.ArrayList;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,6 +21,21 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.jwkim.idus"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(apiInfo());
     }
+    
+    
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "백패커/아이디어스 ",
+                "백엔드 개발 과제 APIs",
+                "v0.1",
+                "localhost",
+                new Contact("Contact us", "-", "roi.joongwon.kim@gmail.com"),
+                "Licenses",
+                "-",
+                new ArrayList<>());
+    }
+
 }
